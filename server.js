daafefc787ac268ff2472707f61c1c8be882df56
William Dunston <wedunsto@aggies.ncat.edu>
2024-01-30 21:18:41 -0500

    Confirmed the express JS web server runs

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log("test");
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3500;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
