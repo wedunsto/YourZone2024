dd6cbaab264c6072c8562bfeb7b034f633d6718c
William Dunston <wedunsto@aggies.ncat.edu>
2024-01-30 22:02:18 -0500

    Confirmed connection to MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-
+const connectDB = require('./config/dbConn');
 const PORT = process.env.PORT || 3500;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Connect to MongoDB
+connectDB();
+
+// If our connection to the database fails, we dont want to listen for connections
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
