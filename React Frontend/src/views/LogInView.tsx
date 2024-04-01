3551cfbe9249ed02926a1952ed324a3e7bf6a019
William Dunston <wedunsto@aggies.ncat.edu>
2024-04-01 16:13:36 -0400

    Began working on Login css

--- React Frontend/src/views/LogInView.tsx
+++ React Frontend/src/views/LogInView.tsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header";
 import LogInForm from "../components/LogInForm";
+import "../styles/LogInStyles.css";
 
 const LogInView = () => {
     return (
-        <div className="h-screen w-screen">
+        <div className="login-background h-screen w-screen">
+            <div className="login-header"></div>
             <div className="flex justify-center">
                 <Header />
             </div>
