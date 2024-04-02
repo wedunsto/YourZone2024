5042f1fb2f8fd20b2b00ec28ead9c551445a6308
William Dunston <wedunsto@aggies.ncat.edu>
2024-04-02 13:39:44 -0400

    Finished setting the login banner

--- React Frontend/src/views/LogInView.tsx
+++ React Frontend/src/views/LogInView.tsx
@@ -1,11 +1,20 @@
+import "../styles/LogInStyles.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookBible } from '@fortawesome/free-solid-svg-icons';
 import Header from "../components/Header";
 import LogInForm from "../components/LogInForm";
-import "../styles/LogInStyles.css";
+import LogInMenu from "../components/LogInMenu";
 
 const LogInView = () => {
     return (
         <div className="login-background h-screen w-screen">
             <div className="login-header"></div>
+            <div className="login-logo">
+                <FontAwesomeIcon icon={faBookBible}/>
+                <p className="login-logo-text">YourZone</p>
+                <LogInMenu />
+            </div>
+            
             <div className="flex justify-center">
                 <Header />
             </div>
