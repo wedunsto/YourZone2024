dc0cd2d8e2d8c1b2544ae2f43c7f1c58e0abbf26
William Dunston <wedunsto@aggies.ncat.edu>
2024-04-18 16:46:10 -0400

    Progressing Login Style

--- React Frontend/src/views/LogInView.tsx
+++ React Frontend/src/views/LogInView.tsx
@@ -1,9 +1,8 @@
 import "../styles/LogInStyles.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBookBible } from '@fortawesome/free-solid-svg-icons';
-import Header from "../components/Header";
-import LogInForm from "../components/LogInForm";
 import LogInMenu from "../components/LogInMenu";
+import BibleStudy from "../../assets/images/BibleStudy.webp";
 
 const LogInView = () => {
     return (
@@ -14,11 +13,16 @@ const LogInView = () => {
                 <p className="login-logo-text">YourZone</p>
                 <LogInMenu />
             </div>
-            
-            <div className="flex justify-center">
-                <Header />
+            <div className="description-contain">
+                <p className="page-description">Empowering your spiritual, financial, and fitness journey</p>
+                <p className="page-sub-description">Track your goals like never before with our innovative online platform.</p>
             </div>
-            <LogInForm />
+            <div className="get-started-container">
+                <button className="get-started">Get Started</button>
+            </div>
+            <div>
+                <img className="preview" src={BibleStudy} />
+            </div>
         </div>
     );
 }
