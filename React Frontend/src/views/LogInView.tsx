0a323d1deec69b4669e3c619d0241f42a17b7e8d
William Dunston <wedunsto@aggies.ncat.edu>
2024-04-18 23:07:02 -0400

    progress

--- React Frontend/src/views/LogInView.tsx
+++ React Frontend/src/views/LogInView.tsx
@@ -7,11 +7,12 @@ import BibleStudy from "../../assets/images/BibleStudy.webp";
 const LogInView = () => {
     return (
         <div className="login-background h-screen w-screen">
-            <div className="login-header"></div>
-            <div className="login-logo">
-                <FontAwesomeIcon icon={faBookBible}/>
-                <p className="login-logo-text">YourZone</p>
-                <LogInMenu />
+            <div className="login-header">
+                <div className="login-logo">
+                    <FontAwesomeIcon icon={faBookBible}/>
+                    <p className="login-logo-text">YourZone</p>
+                    <LogInMenu />
+                </div>
             </div>
             <p className="page-description">Empowering your spiritual, financial, and fitness journey</p>
             <p className="page-sub-description">Track your goals like never before with our innovative online platform.</p>
