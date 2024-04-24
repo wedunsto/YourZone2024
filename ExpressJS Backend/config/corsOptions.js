61912073403948c4bae314a590df78ed88a004a9
William Dunston <wedunsto@aggies.ncat.edu>
2024-04-24 16:00:55 -0400

    i hate it

--- ExpressJS Backend/config/corsOptions.js
+++ ExpressJS Backend/config/corsOptions.js
@@ -3,8 +3,7 @@ const allowedOrigins = require('./allowedOrigins.js');
 const corsOptions = {
     origin: (origin, callback) => {
         // only do the OR during development
-        // if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        if (allowedOrigins.indexOf(origin) !== -1) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true);
         } else {
             callback(new Error('not allowed by CORS'));
