dd61ef33b4c9764cc76b21778e8d150b695cd1eb
William Dunston <wedunsto@aggies.ncat.edu>
2024-04-24 14:32:25 -0400

    fix errors

--- ExpressJS Backend/middleware/ErrorHandler.js
+++ ExpressJS Backend/middleware/ErrorHandler.js
@@ -1,6 +1,6 @@
 const { logEvents } = require('./logEvents');
 
-const errorHandler = (err, req, res, next) => {
+const errorHandler = (err, res) => {
     logEvents(`${err.name}: ${err.message}`, 'errLog.txt');
     console.error(err.stack);
     res.status(500).send(err.message);
