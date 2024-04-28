1ce43896b42b2bc92065d736f3fbe56ff8ad4f2d
William Dunston <wedunsto@aggies.ncat.edu>
2024-04-28 16:50:42 -0400

    adjusted whitelist

--- ExpressJS Backend/server.js
+++ ExpressJS Backend/server.js
@@ -21,7 +21,11 @@ app.use(credentials);
 //app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://yourzone.hopto.org');
+  const allowedOrigins = ['http://yourzone.hopto.org', 'http://localhost:5173'];
+  const origin = req.headers.origin;
+  if(allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (req.method === 'OPTIONS') {
