988c83f123e6d6b43cb83aea0c38d9cdda7ca1fc
William Dunston <wedunsto@aggies.ncat.edu>
2024-10-27 15:50:02 -0400

    test

--- ExpressJS Backend/config/allowedOrigins.js
+++ ExpressJS Backend/config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
 	'https://yourzone.hopto.org',
     'http://localhost:8080',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'http://localhost:5173'
 ];
 
     module.exports = allowedOrigins;
